ce14a021af25ddb5cefe2980bb745114269de2e5
JarPr <131611487+JarPr@users.noreply.github.com>
2023-10-30 14:28:22 +0100

    react-hw-01

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
+
+import user from '../data/user.json';
+import { Profile } from './Profile/Profile';
+import data from '../data/data.json';
+import { Statistics } from './Statistics/Statistics';
+import { generateColors} from './Statistics/Statistics.styled'
+import friends from '../data/friends.json'
+import { FriendList } from './FriendList/FriendList';
+import transactions from '../data/transactions.json';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory'
 export const App = () => {
+const statsColors = generateColors(data)
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile card={user}/>
+      <Statistics title="Upload stats" stats={statsColors} />
+      <FriendList friends={friends}/>
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
+ 
---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,21 @@
+
+import { ListStats, LiItemStats, LabelStats, PercentageStats , TitleStats, ContainerStates} from "./Statistics.styled";
+
+export  const Statistics = ({title, stats}) => {
+    return(
+      <ContainerStates>
+{title && <TitleStats>{title}</TitleStats>}
+      <ListStats>
+      {stats.map(item => {
+        return (
+          <LiItemStats key={item.id} backgroundColor={item.backgroundColor}>
+             <LabelStats>{item.label}</LabelStats>
+            <PercentageStats>{item.percentage}<b>%</b></PercentageStats>
+          </LiItemStats>
+        );
+      })}
+    </ListStats>
+    </ContainerStates>
+    )
+    
+  };
